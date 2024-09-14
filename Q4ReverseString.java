3ce48ce6b8fb2fdba6f56774913b9acbe7159c8d
LuisXP22 <70615568+LuisXP22@users.noreply.github.com>
2024-09-13 23:57:15 -0400

    Update Q4ReverseString.java

--- Q4ReverseString.java
+++ Q4ReverseString.java
@@ -1,14 +1,11 @@
-public class Q4ReverseString {
-    public static void main(String[] args) {
-        String input = "test string";
-        String reversed = reverse(input);
-        System.out.println(reversed); 
+function reverse(s) {
+    if (s === "") {
+        return s;
     }
-    public static String reverse(String s) {
-        if (s.isEmpty()) {
-            return s;
-        }
-      
-        return reverse(s.substring(1)) + s.charAt(0);
-    }
+
+    return reverse(s.substring(1)) + s.charAt(0);
 }
+
+const input = "test string";
+const reversed = reverse(input);
+console.log(reversed);
